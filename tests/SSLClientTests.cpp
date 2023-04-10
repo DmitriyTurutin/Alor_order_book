2fa66afd2dc36b6cb8ac81ba156d55b525383394
Dima Turutin <qwe068776@gmail.com>
2023-04-10 23:49:54 +0300

    Init reafctor commit: :

--- tests/SSLClientTests.cpp
+++ tests/SSLClientTests.cpp
@@ -5,3 +5,11 @@ TEST(SSLClientTest, ConnectTest) {
     SSLClient client("www.example.com", "443");
     EXPECT_NO_THROW(client.connect());
 }
+TEST(SSLClientTest, RequestTest) {
+    SSLClient client("www.example.com", "443");
+    client.connect();
+    std::string response = client.request("GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n");
+    ASSERT_FALSE(response.empty());
+}
+
+TEST(SSLClientTest, )
