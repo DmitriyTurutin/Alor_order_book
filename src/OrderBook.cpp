dd4b959f65899b0a25d0bd268b8eec85f2126e12
Dima Turutin <qwe068776@gmail.com>
2023-04-11 19:03:40 +0300

    Get dpeth snapshot,TODO: should also extract needed information

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -4,7 +4,7 @@ OrderBook::OrderBook()
         : ssl_client(std::make_unique<SSLClient>("fstream.binance.com","443")) {
     
     ssl_client->connect();
-    depth_snapshot();
+    std::cout << depth_snapshot();
 }
 
 void OrderBook::run_forever(std::string& message) {
@@ -15,35 +15,64 @@ void OrderBook::run_forever(std::string& message) {
 }
 
 std::vector<OrderBook::Level> OrderBook::parse_json(std::string json) {
+    return std::vector<OrderBook::Level>();
 }
 
 std::string OrderBook::depth_snapshot() {
-    // Send get request to https://fapi.binance.com/fapi/v1/depth?symbol=BTCUSDT&limit=1000
-    // Create context
-    using boost::asio::ip::tcp;
-    
+    // Send GET request to https://fapi.binance.com/fapi/v1/depth?symbol=BTCUSDT&limit=1000
     boost::asio::io_context io_context;
 
-    // Resolve the hostname
+    // Resolve the hostname and port
     tcp::resolver resolver(io_context);
     tcp::resolver::results_type endpoints =
-        resolver.resolve("example.com", "http");
+            resolver.resolve("fapi.binance.com", "443");
 
-    // Connect to the server
-    tcp::socket socket(io_context);
-    boost::asio::connect(socket, endpoints);
+    // Establish a secure SSL/TLS connection to the server
+    boost::asio::ssl::context ssl_context(boost::asio::ssl::context::tlsv12_client);
+    boost::asio::ssl::stream<tcp::socket> ssl_socket(io_context, ssl_context);
+    boost::asio::connect(ssl_socket.lowest_layer(), endpoints);
+    ssl_socket.handshake(boost::asio::ssl::stream_base::handshake_type::client);
 
-    // Send the HTTP request
-    boost::asio::streambuf request;
-    std::ostream request_stream(&request);
-    request_stream << "GET /index.html HTTP/1.1\r\n"
-                   << "Host: example.com\r\n"
-                   << "Connection: close\r\n\r\n";
-    boost::asio::write(socket, request);
+    // Send an HTTPS GET request with the query parameters
+    std::string request =
+            "GET /fapi/v1/depth?symbol=BTCUSDT&limit=10 HTTP/1.1\r\n"
+            "Host: fapi.binance.com\r\n"
+            "Accept: */*\r\n"
+            "Connection: close\r\n"
+            "\r\n";
+    boost::asio::write(ssl_socket, boost::asio::buffer(request));
 
+    // Read the response and print it out
+    boost::asio::streambuf response_buffer;
+    boost::asio::read_until(ssl_socket, response_buffer, "\r\n");
+    std::istream response_stream(&response_buffer);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+    if (!response_stream || http_version.substr(0, 5) != "HTTP/") {
+        std::cerr << "Error: Invalid response\n";
+    }
+    if (status_code != 200) {
+        std::cerr << "Error: Server returned status code " << status_code << "\n";
+    }
+    boost::asio::read_until(ssl_socket, response_buffer, "\r\n\r\n");
+    std::string header;
+    boost::system::error_code error;
+    while (std::getline(response_stream, header) && header != "\r") {}
+    std::stringstream response_body_stream;
+    while (boost::asio::read(ssl_socket, response_buffer,
+                             boost::asio::transfer_at_least(1), error)) {
+        response_body_stream << &response_buffer;
+    }
+    if (error != boost::asio::error::eof) {
+        std::cerr << "Error: " << error.message() << "\n";
+    }
+    std::string response_body = response_body_stream.str();
 
-    return std::string();
-    
+    return response_body;
 }
 
 
